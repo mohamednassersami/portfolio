e35983f923a3da1cbf0578af1388f4d6b14cfbc2
mohamednasser <mohamed.nasser.sami.97@gmail.com>
2023-10-15 16:27:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Main from "./components/main/Main";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+
+      <Hero />
+      <div className="divider" />
+      <Main />
+      <div className="divider" />
+      <Contact />
+      <div className="divider" />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
